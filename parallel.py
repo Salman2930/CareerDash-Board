4481b92603da8d38e2c13023ff9ff8debb49bfde
salman <124383545+Salman2930@users.noreply.github.com>
2023-11-28 16:05:59 +0530

    Add files via upload

--- parallel.py
+++ parallel.py
@@ -3,22 +3,20 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
-def get_parallel():
+url = 'https://jobs.lever.co/parallelwireless?location=Bangalore'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html5lib')
 
-    url = 'https://jobs.lever.co/parallelwireless?location=Bangalore'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html5lib')
+soup_data = soup.find('div' , {'class' : 'postings-wrapper'}).find('div' , {'class' : 'postings-group'}).find_all('div' , {'class' : 'posting'})
+final_data = []
 
-    soup_data = soup.find('div' , {'class' : 'postings-wrapper'}).find('div' , {'class' : 'postings-group'}).find_all('div' , {'class' : 'posting'})
-    final_data = []
-
-    for cat in soup_data:
-        content = {}
-        
-        content['Job Title'] = cat.find('a' , {'class' : 'posting-title'}).find('h5').text
-        content['Job Location'] = cat.find('div' , {'class' : 'posting-categories'}).find('span').text
-        content['Aplly Link'] = cat.find('a' , {'class' : 'posting-title'})['href']
-        final_data.append(content)
-        
-    parallel_df_data = pd.DataFrame(final_data)
-    return parallel_df_data
+for cat in soup_data:
+    content = {}
+    
+    content['Job Title'] = cat.find('a' , {'class' : 'posting-title'}).find('h5').text
+    content['Job Location'] = cat.find('div' , {'class' : 'posting-categories'}).find('span').text
+    content['Aplly Link'] = cat.find('a' , {'class' : 'posting-title'})['href']
+    final_data.append(content)
+    
+parallel_df_data = pd.DataFrame(final_data)
+parallel_df_data
